db9d1a10aae45d3dbe73ce3817e2b03ba189ada1
PARI-IT-2\PARI <pari_20@superpari.co.id>
2026-04-09 19:23:19 +0700

    fix int parent departement

--- internal/departement/models/department.go
+++ internal/departement/models/department.go
@@ -5,13 +5,15 @@ import (
 )
 
 type Department struct {
-	ID                 int64     `json:"id" db:"id"`
-	DepartmentCode     string    `json:"department_code" db:"department_code"`
-	DepartmentName     string    `json:"department_name" db:"department_name"`
-	Description        *string   `json:"description,omitempty" db:"description"`
-	ManagerID          *int64    `json:"manager_id,omitempty" db:"manager_id"`
-	ParentDepartmentID *string   `json:"parent_department_id,omitempty" db:"parent_department_id"`
-	Status             string    `json:"status" db:"status"`
-	CreatedAt          time.Time `json:"created_at" db:"created_at"`
-	UpdatedAt          time.Time `json:"updated_at" db:"updated_at"`
+	ID                 int64     `json:"id"`
+	DepartmentCode     string    `json:"department_code"`
+	DepartmentName     string    `json:"department_name"`
+	Description        *string   `json:"description,omitempty"`
+	ManagerID          *int64    `json:"manager_id,omitempty"`
+	ParentDepartmentID *int64    `json:"parent_department_id,omitempty"`
+	Status             string    `json:"status"`
+	CreatedAt          time.Time `json:"created_at"`
+	UpdatedAt          time.Time `json:"updated_at"`
+
+	Parent *Department `json:"parent,omitempty" gorm:"-"`
 }
