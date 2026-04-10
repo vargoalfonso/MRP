b7fa0ceefea9d0a7e071e81d95b2f07ba07b078d
PARI-IT-2\PARI <pari_20@superpari.co.id>
2026-04-10 21:24:17 +0700

    po split

--- internal/po_split_setting/models/request.go
+++ internal/po_split_setting/models/request.go
@@ -5,10 +5,12 @@ type CreatePOSplitRequest struct {
 	MinOrderQty   int    `json:"min_order_qty" binding:"required"`
 	MaxSplitLines int    `json:"max_split_lines" binding:"required"`
 	SplitRule     string `json:"split_rule" binding:"required"`
+	Status        string `json:"status" binding:"required"`
 }
 
 type UpdatePOSplitRequest struct {
 	MinOrderQty   int    `json:"min_order_qty"`
 	MaxSplitLines int    `json:"max_split_lines"`
 	SplitRule     string `json:"split_rule"`
+	Status        string `json:"status"`
 }
