0ce849a414a30c5cd956a689555d226b37356aca
yosuanetzme18 <126744589+yosuanetzme18@users.noreply.github.com>
2026-05-20 21:01:08 +0700

    update process parameter model is assembly

--- internal/process_parameter/models/model.go
+++ internal/process_parameter/models/model.go
@@ -5,16 +5,16 @@ import (
 )
 
 type ProcessParameter struct {
-	ID          int64     `gorm:"primaryKey"`
-	ProcessCode string    `gorm:"column:process_code"`
-	ProcessName string    `gorm:"column:process_name"`
-	Category    string    `gorm:"column:category"`
-	Sequence    int       `gorm:"column:sequence"`
-	Status      string    `gorm:"column:status"`
-	Subcon      bool      `gorm:"column:sub_con;default:false"`
-	IsAssembly  bool      `gorm:"column:is_assembly;default:false"`
-	CreatedAt   time.Time `gorm:"column:created_at"`
-	UpdatedAt   time.Time `gorm:"column:updated_at"`
+	ID          int64     `gorm:"primaryKey"                       json:"id"`
+	ProcessCode string    `gorm:"column:process_code"              json:"process_code"`
+	ProcessName string    `gorm:"column:process_name"              json:"process_name"`
+	Category    string    `gorm:"column:category"                  json:"category"`
+	Sequence    int       `gorm:"column:sequence"                  json:"sequence"`
+	Status      string    `gorm:"column:status"                    json:"status"`
+	Subcon      bool      `gorm:"column:sub_con;default:false"     json:"sub_con"`
+	IsAssembly  bool      `gorm:"column:is_assembly;default:false" json:"is_assembly"`
+	CreatedAt   time.Time `gorm:"column:created_at"                json:"created_at"`
+	UpdatedAt   time.Time `gorm:"column:updated_at"                json:"updated_at"`
 }
 
 func (ProcessParameter) TableName() string {
