968e91d76f18d0b66220222de93347944dfbf2a0
PARI-IT-2\PARI <pari_20@superpari.co.id>
2026-06-23 17:33:28 +0700

    change validasi process

--- internal/process_parameter/models/request.go
+++ internal/process_parameter/models/request.go
@@ -3,8 +3,8 @@ package models
 type CreateProcessRequest struct {
 	ProcessCode string `json:"process_code" validate:"required"`
 	ProcessName string `json:"process_name" validate:"required"`
-	Category    string `json:"category" validate:"required"`
-	Sequence    int    `json:"sequence" validate:"required"`
+	Category    string `json:"category"`
+	Sequence    int    `json:"sequence"`
 	Status      string `json:"status" validate:"required"`
 	Subcon      bool   `json:"sub_con"`
 	IsAssembly  bool   `json:"is_assembly"`
